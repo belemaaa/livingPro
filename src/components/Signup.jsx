f7c7ef5a143b3acbbe3cdb19108e3846037d6b0e
belemaaa <graceitamunoala@gmail.com>
2023-07-17 17:19:14 +0100

    added signup page

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import {IoIosArrowBack} from 'react-icons/io'
+import { Link } from 'react-router-dom'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div>
+      <div>
+        <Link to='/onboarding3'>
+            <IoIosArrowBack className='signup-go-back-btn'/>
+        </Link>
+      </div>
+
+      <div>
+        <p>Welcome!!</p>
+        <p>Kindly fill in these details to create an account ad find what you need</p>
+      </div>
+
+      <div>
+        <form method='POST' className='signup-form'>
+          <input type='text' name='name' placeholder='Enter your Full name'/>
+        </form>
+      </div>
+    </div>
   )
 }
 

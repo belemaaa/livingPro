0db7e339f8784a0cf08e69a236851a5fe56801d5
belemaaa <graceitamunoala@gmail.com>
2023-07-17 17:58:48 +0100

    styling signup component

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../assets/css/styles.css'
 import {IoIosArrowBack} from 'react-icons/io'
 import { Link } from 'react-router-dom'
 import GoogleFrame from '../assets/images/GoogleFrame.png'
@@ -6,18 +7,18 @@ import GoogleFrame from '../assets/images/GoogleFrame.png'
 const Signup = () => {
   return (
     <div>
-      <div>
+      <div className='signup-go-back-btn'>
         <Link to='/onboarding3'>
-            <IoIosArrowBack className='signup-go-back-btn'/>
+            <IoIosArrowBack size={30}/>
         </Link>
       </div>
 
-      <div>
-        <p>Welcome!!</p>
-        <p>Kindly fill in these details to create an account ad find what you need</p>
+      <div className='signup-head'>
+        <p className='signup-welcome'>Welcome!!</p>
+        <p className='signup-head-p'>Kindly fill in these details to create an account ad find what you need</p>
       </div>
 
-      <div>
+      <div className='signup-form-div'>
         <form method='POST' className='signup-form'>
           <input type='text' name='name' placeholder='Enter your Full name' required/>
           <input type='email' name='email' placeholder='Enter your Email Address' required/>
@@ -30,17 +31,17 @@ const Signup = () => {
         </form>
       </div>
 
-      <div>
-        <p>You can create an account with</p>
-        <div>
+      <div className='google-auth-div'>
+        <p className='google-auth-p'>You can create an account with</p>
+        <div className='google-auth'>
           <Link to='#'>
-            <img src={GoogleFrame}/>
+            <img src={GoogleFrame} className='google-frame'/>
           </Link>
         </div>
       </div>
 
-      <p>Not new here? Please 
-        <span>
+      <p className='login-option'>Not new here? Please 
+        <span className='login'>
         <Link to='/login'>Log in</Link>
         </span>
       </p>

67575b8dddb24f6b015dd5a1adeb151d848149ef
belemaaa <graceitamunoala@gmail.com>
2023-07-17 17:32:13 +0100

    signup

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {IoIosArrowBack} from 'react-icons/io'
 import { Link } from 'react-router-dom'
+import GoogleFrame from '../assets/images/GoogleFrame.png'
 
 const Signup = () => {
   return (
@@ -18,9 +19,31 @@ const Signup = () => {
 
       <div>
         <form method='POST' className='signup-form'>
-          <input type='text' name='name' placeholder='Enter your Full name'/>
+          <input type='text' name='name' placeholder='Enter your Full name' required/>
+          <input type='email' name='email' placeholder='Enter your Email Address' required/>
+          <input type='password' name='password' placeholder='Eter your Password' required/>
+
+          <label for='Remember me'>Remember me</label>
+          <input type='checkbox' name='RememberMe'/>
+
+          <button type='submit'>Create Account</button>
         </form>
       </div>
+
+      <div>
+        <p>You can create an account with</p>
+        <div>
+          <Link to='#'>
+            <img src={GoogleFrame}/>
+          </Link>
+        </div>
+      </div>
+
+      <p>Not new here? Please 
+        <span>
+        <Link to='/login'>Log in</Link>
+        </span>
+      </p>
     </div>
   )
 }

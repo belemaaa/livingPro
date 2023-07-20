41b08e3182305ee1f6c32f51f53cba6ec724650a
belemaaa <graceitamunoala@gmail.com>
2023-07-20 13:19:23 -0700

    personality list

--- src/components/CompatibilityTest1.jsx
+++ src/components/CompatibilityTest1.jsx
@@ -4,6 +4,28 @@ import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom';
 
 const CompatibilityTest1 = () => {
+    const personalityTestBtns = [
+        {id:1, text:'Patient'},
+        {id:2, text:'Funny'},
+        {id:3, text:'Strict'},
+        {id:4, text:'Organized'},
+        {id:5, text:'Neat'},
+        {id:6, text:'Straightforward'},
+        {id:7, text:'Trustworthy'},
+        {id:8, text:'Creative'},
+        {id:9, text:'Introvert'},
+        {id:10, text:'Extrovert'},
+        {id:11, text:'Optimistc'},
+        {id:12, text:'Egocentric'},
+        {id:13, text:'Self aware'},
+        {id:14, text:'Honest'},
+        {id:15, text:'Generous'},
+        {id:16, text:'Logical'},
+        {id:17, text:'Emotional'},
+        {id:18, text:'Stubborn'},
+        {id:19, text:'Playful'},
+    ]
+
   return (
     <div>
         <div className='prev-next-options'>

b466fc872442154aa30e45b2511a173161ececbc
belemaaa <graceitamunoala@gmail.com>
2023-07-20 10:58:24 -0700

    form

--- src/components/AboutInfo.jsx
+++ src/components/AboutInfo.jsx
@@ -29,66 +29,91 @@ const AboutInfo = () => {
 
         <div>
           <form method='POST'>
-            <label className='about-form-label'>What's your full name? 
-            <span className='required-asterik'>*</span></label>
+            <div className='about-form-div'>
+              <label className='about-form-label'>What's your full name? 
+              <span className='required-asterik'>*</span></label>
 
-            <input type='text' name='name' 
-            className='about-form-input-box border'
-            required/>
+              <input type='text' name='name' 
+              className='about-form-input-box border'
+              required/>
+            </div>
+            
+            <br/>
+
+            <div className='about-form-div'>
+              <label className='about-form-label'>What do you do? (Occupation) 
+              <span className='required-asterik'>*</span></label>
+
+              <input type='text' name='occupation' 
+              className='about-form-input-box border'
+              required/>
+            </div>     
 
             <br/>
-            <label className='about-form-label'>What do you do? (Occupation) 
-            <span className='required-asterik'>*</span></label>
 
-            <input type='text' name='occupation' 
-            className='about-form-input-box border'
-            required/>
-
-            <br/>
-            <label className='about-form-label-A'>Age? 
-            <span className='required-asterik'>*</span></label>
-
-            <input type='text' name='age' 
-            className='about-form-input-box-A border'
-            required/>
-
-            <label className='about-form-label-D'>Date of Birth 
-            <span className='required-asterik'>*</span></label>
-
-            <input type='date' 
-            name='dateOfBirth' 
-            className='about-form-input-box-D border'
-            required/>
-
-            <br/>
-            <label className='about-form-label'>Location 
-            <span className='required-asterik'>*</span></label>
-
-            <select name='location' 
-            className='about-form-input-box border' 
-            value={selectedLocation} 
-            onChange={handleLocationChange}
-            required>
-              <option value=""></option>
-                {nigerianStates.map((state) => (
-                  <option key={state} value={state}>
-                    {state}
-                  </option>
-                ))}
-            </select>
+            <div className='AD'>
+              <div>
+                <label className=''>Age? 
+                  <span className='required-asterik'>*</span>
+                </label>
+                <br/>
+                <input type='text' name='age' 
+                className='about-form-input-box-A border'
+                required/>
+              </div>
+              
+              <div>
+                <label className=''>Date of Birth 
+                <span className='required-asterik'>*</span></label>
+                <br/>
+                <input type='date' 
+                name='dateOfBirth' 
+                className='about-form-input-box-D border'
+                required/>
+                </div>
+            </div>
 
             <br/>
-            <label className='about-form-label'>Mobile number</label>
-            <input type='phone' 
-            name='phone' 
-            className='about-form-input-box border'
-            />
+
+            <div className='about-form-div'>
+              <label className='about-form-label'>Location 
+              <span className='required-asterik'>*</span></label>
+
+              <select name='location' 
+              className='about-form-input-box border' 
+              value={selectedLocation} 
+              onChange={handleLocationChange}
+              required>
+                <option value=""></option>
+                  {nigerianStates.map((state) => (
+                    <option key={state} value={state}>
+                      {state}
+                    </option>
+                  ))}
+              </select>
+            </div>
+           
 
             <br/>
-            <label className='about-form-label'>Here for:</label>
-            <button className='here-for-option-btn'>Find apartment</button>
-            <button className='here-for-option-btn'>Find a roomate</button>
-            <button className='here-for-option-btn'>Post apartment</button>
+
+            <div className='about-form-div'>
+              <label className='about-form-label'>Mobile number</label>
+              <input type='phone' 
+              name='phone' 
+              className='about-form-input-box border'
+              />
+            </div>
+           
+
+            <br/>
+
+            <div className='about-form-div'>
+              <label className='about-form-label'>Here for:</label>
+              <button className='here-for-option-btn'>Find apartment</button>
+              <button className='here-for-option-btn'>Find a roomate</button>
+              <button className='here-for-option-btn'>Post apartment</button>
+            </div>
+            
 
             <button type='submit' className='about-proceed-btn'
             >

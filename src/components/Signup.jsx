95d372a70604ff5c53a9ed1968150a5be22b0b50
belemaaa <graceitamunoala@gmail.com>
2023-07-23 23:21:57 -0700

    md queries

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -43,80 +43,85 @@ const Signup = () => {
   };
 
   return (
-    <div className='sign-page'>
+    <div className='signup-page'>
       <div className='signup-go-back-btn'>
         <Link to='/onboarding3'>
             <IoIosArrowBack size={25}/>
         </Link>
       </div>
 
-      <div className='signup-head'>
-        <p className='signup-welcome'>Welcome!!</p>
-        <p className='signup-head-p'>Kindly fill in these details to create an account and find what you need</p>
-      </div>
-
-      <div className='signup-form-div'>
-        <form method='POST' className='signup-form' onSubmit={handleCreateAccount}>
-          <input 
-            type='text' 
-            value={fullname}
-            onChange={(e) => setFullname(e.target.value)}
-            placeholder='Enter your Full name' 
-            required
-            className='signup-form-input-box border'
-            autoComplete='off'
-          />
-
-          <input type='email' 
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder='Enter your Email Address' 
-            required
-            className='signup-form-input-box border'
-            autoComplete='off'
-          />
-
-          <input type='password' 
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder='Enter your Password' 
-            required
-            className='signup-form-input-box border'
-            autoComplete='off'
-          />
-
-          <div className='flex flex-row'>
-            <label for='Remember me' className='RememberMe'>
-              Remember me
-            </label>
-            <div className='RM-checkbox'>
-              <input type='checkbox' name='RememberMe'/>
-            </div>
-          </div>
-
-          <br/>
-          <button type='submit' className='create-account-btn'>
-            Create Account
-          </button>
-        </form>
-      </div>
-
-      <div className='google-auth-div'>
-        <p className='google-auth-p'>
-          You can create an account with
-        </p>
-        <div>
-          <Link to='#'>
-            <img src={GoogleFrame} className='google-frame'/>
-          </Link>
+      <div className='signup-div'>
+        <div className='signup-head'>
+          <p className='signup-welcome'>Welcome!!</p>
+          <p className='signup-head-p'>Kindly fill in these details to create an account and find what you need</p>
         </div>
-      </div>
 
-      <p className='login-option'>Not new here? Please 
-        <span className='login'>
-        <Link to='/login' className='signup-login-link'> Log in</Link>
-        </span>
-      </p>
+        <div className='signup-form-div'>
+          <form method='POST' className='signup-form' onSubmit={handleCreateAccount}>
+            <input 
+              type='text' 
+              value={fullname}
+              onChange={(e) => setFullname(e.target.value)}
+              placeholder='Enter your Full name' 
+              required
+              className='signup-form-input-box border'
+              autoComplete='off'
+            />
+            
+            <br/>
+            <input type='email' 
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder='Enter your Email Address' 
+              required
+              className='signup-form-input-box border'
+              autoComplete='off'
+            />
+
+            <br/>
+            <input type='password' 
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder='Enter your Password' 
+              required
+              className='signup-form-input-box border'
+              autoComplete='off'
+            />
+
+            <div className='flex flex-row'>
+              <label for='Remember me' className='RememberMe'>
+                Remember me
+              </label>
+              <div className='RM-checkbox'>
+                <input type='checkbox' name='RememberMe'/>
+              </div>
+            </div>
+
+            <br/>
+            <button type='submit' className='create-account-btn'>
+              Create Account
+            </button>
+          </form>
+        </div>
+
+        <div className='google-auth-div'>
+          <p className='google-auth-p'>
+            You can create an account with
+          </p>
+          <div>
+            <Link to='#'>
+              <img src={GoogleFrame} className='google-frame'/>
+            </Link>
+          </div>
+        </div>
+
+        <p className='login-option'>Not new here? Please 
+          <span className='login'>
+          <Link to='/login' className='signup-login-link'> Log in</Link>
+          </span>
+        </p>
+      </div>
+      
 
       
     </div>

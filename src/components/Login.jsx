10bf0303ac1931dc248ce181edee3128f0f06e18
belemaaa <graceitamunoala@gmail.com>
2023-07-24 10:27:30 -0700

    media queries

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -44,63 +44,66 @@ const Login = () => {
           </Link>
         </div>
   
-        <div className='signup-head'>
-          <p className='signup-welcome'>Welcome!!</p>
-          <p className='signup-head-p'>Kindly fill in these details to create an account and find what you need</p>
-        </div>
-  
-        <div className='signup-form-div'>
-          <form method='POST' className='signup-form' onSubmit={handleLogin}>
-            <input type='email' 
-            value={username} 
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder='Enter your Email Address' 
-            required
-            className='signup-form-input-box border'
-            autoComplete='off'
-            />
-  
-            <br/>
-            <input type='password' 
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder='Enter your Password' 
-            required
-            className='signup-form-input-box border'
-            autoComplete='off'
-            />
-  
-            <div className='forgot-password-btn'>
-                <Link to='#'>
-                    Forgot Passoword?
-                </Link>
-            </div>
-  
-            <br/>
-            <button type='submit' className='create-account-btn'
-            >
-                Log in
-            </button>
-          </form>
-        </div>
-  
-        <div className='google-auth-div'>
-          <p className='google-auth-p'>
-            You can log in with
-          </p>
-          <div>
-            <Link to='#'>
-              <img src={GoogleFrame} className='google-frame'/>
-            </Link>
+        <div className='login-div'>
+          <div className='signup-head'>
+            <p className='signup-welcome'>Welcome!!</p>
+            <p className='signup-head-p'>Kindly fill in these details to create an account and find what you need</p>
           </div>
+    
+          <div className='signup-form-div'>
+            <form method='POST' className='signup-form' onSubmit={handleLogin}>
+              <input type='email' 
+              value={username} 
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder='Enter your Email Address' 
+              required
+              className='signup-form-input-box border'
+              autoComplete='off'
+              />
+    
+              <br/>
+              <input type='password' 
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder='Enter your Password' 
+              required
+              className='signup-form-input-box border'
+              autoComplete='off'
+              />
+    
+              <div className='forgot-password-btn'>
+                  <Link to='#'>
+                      Forgot Passoword?
+                  </Link>
+              </div>
+    
+              <br/>
+              <button type='submit' className='create-account-btn'
+              >
+                  Log in
+              </button>
+            </form>
+          </div>
+    
+          <div className='google-auth-div'>
+            <p className='google-auth-p'>
+              You can log in with
+            </p>
+            <div>
+              <Link to='#'>
+                <img src={GoogleFrame} className='google-frame'/>
+              </Link>
+            </div>
+          </div>
+    
+          <p className='create-account-option'>New here? Please 
+            <span className=''>
+            <Link to='/signup' className='signup-create-account-link'> Create an Account</Link>
+            </span>
+          </p>
+  
         </div>
-  
-        <p className='create-account-option'>New here? Please 
-          <span className=''>
-          <Link to='/signup' className='signup-create-account-link'> Create an Account</Link>
-          </span>
-        </p>
-  
+        
         
       </div>
     )

8cf37a698cda90d65e6ac626d89e705a56909513
belemaaa <graceitamunoala@gmail.com>
2023-07-27 11:49:37 +0000

    profile page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,6 +3,15 @@ import ProfileFooter from './ProfileFooter';
 import woman from '../assets/images/womanDP.png'
 import verified from '../assets/images/ic_baseline-verified.png'
 import {CiLocationOn} from 'react-icons/ci'
+import {RiEditLine} from 'react-icons/ri'
+import {IoIosArrowForward} from 'react-icons/io'
+import {TbSettings} from 'react-icons/tb'
+import matches from '../assets/images/matches.png'
+import notifications from '../assets/images/notifications.png'
+import recentPosts from '../assets/images/recentPosts.png'
+import history from '../assets/images/history.png'
+import logout from '../assets/images/logout.png'
+import blueVerified from '../assets/images/blueVerified.png'
 
 const Profile = () => {
     useEffect(() => {
@@ -22,8 +31,78 @@ const Profile = () => {
         </div>
 
         <div>
-          
+          <div>
+            <RiEditLine/>
+            <p>Edit Compatibilty test</p>
+            <IoIosArrowForward/>
+          </div>
+
+          <div className='profile-border-b'/>
+
+          <div>
+            <img src={matches}/>
+            <p>Matches</p>
+            <IoIosArrowForward/>
+          </div>
+
+          <div className='profile-border-b'/>
+
+          <div>
+            <img src={notifications}/>
+            <p>Notifications</p>
+            <IoIosArrowForward/>
+          </div>
         </div>
+
+        <div className='profile-border-b'/>
+
+        <div>
+          <img src={recentPosts} />
+          <p>Check Recent Posts</p>
+          <IoIosArrowForward/>
+        </div>
+
+        <div className='profile-border-b'/>
+
+        <div>
+          <RiEditLine />
+          <p>Edit Profile</p>
+          <IoIosArrowForward/>
+        </div>
+
+        <div className='profile-border-b'/>
+
+        <div>
+          <img src={history} />
+          <p>History and Reviews</p>
+          <IoIosArrowForward/>
+        </div>
+
+        <div className='profile-border-b'/>
+
+        <div>
+          <TbSettings/>
+          <p>Settings</p>
+          <IoIosArrowForward/>
+        </div>
+
+        <div className='profile-border-b'/>
+
+        <div>
+          <img src={blueVerified} />
+          <p>Verification</p>
+          <IoIosArrowForward/>
+        </div>
+
+        <div className='profile-border-b'/>
+
+        <div>
+          <img src={logout} />
+          <p>Log out</p>
+          <IoIosArrowForward/>
+        </div>
+
+        <div className='profile-border-b'/>
 
         <div className='md:hidden'>
           <ProfileFooter/>

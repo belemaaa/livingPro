bcf4abd57829ab527376bb762168e6607ef9058b
belemaaa <graceitamunoala@gmail.com>
2023-08-02 11:43:11 +0000

    loader

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,56 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import HomeFooter from './HomeFooter';
 import Profile from './Profile';
 import {BsFillHouseHeartFill} from 'react-icons/bs'
+import SquareLoader from "react-spinners/SquareLoader";
 
 const Home = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
+  // loader
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+      setLoading(true)
+      setTimeout(() => {
+          setLoading(false)
+      }, 4000)
+  }, [])
+
+
   return (
     <div className='overflow-hidden'>
-      <div className='home'>
-          <div className='hidden md:flex dk-profile border-r'>
-            <Profile/>
-          </div>
+      {
+        loading ?
+          <SquareLoader
+            color={'#000000'}
+            loading={loading}
+            size={60}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+            className='loader'
+          />
+        :
+          <div>
+            <div className='home'>
+                <div className='hidden md:flex dk-profile border-r'>
+                  <Profile/>
+                </div>
 
-          <div className='home-page'>
-            <div className='home-icon-bg w-screen'>
-              <BsFillHouseHeartFill size={35} className='lp-home-icon'/>
+                <div className='home-page'>
+                  <div className='home-icon-bg w-screen'>
+                    <BsFillHouseHeartFill size={35} className='lp-home-icon'/>
+                  </div>
+                </div>
             </div>
-          </div>
-      </div>
 
-      <HomeFooter/>
+            <HomeFooter/>
+          </div>
+        
+      }
+      
+      
     </div>
   )
 }

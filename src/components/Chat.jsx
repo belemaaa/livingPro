9d0e4be1753bbb379dbd7570d2ebad5fba527c90
belemaaa <graceitamunoala@gmail.com>
2023-08-03 14:43:25 +0000

    chat header

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,6 +1,11 @@
 import React, {useState} from 'react'
 import Profile from './Profile.jsx'
 import InboxFooter from './InboxFooter'
+import {BsTelephone} from 'react-icons/bs'
+import {BsCameraVideo} from 'react-icons/bs'
+import {IoIosArrowBack} from 'react-icons/io'
+
+import { Link } from 'react-router-dom'
 
 const Chat = () => {
   return (
@@ -10,7 +15,23 @@ const Chat = () => {
         </div>
 
         <div className='chat-page'>
-            <p>Chat page</p>
+            <div className='chat-header'>
+                <div className='signup-go-back-btn'>
+                    <Link to='/about'>
+                        <IoIosArrowBack size={25}/>
+                    </Link>
+                </div>
+
+                <div className='chat-header-title'>
+                    <img src='' className='border'/>
+                    <p>Grace Belema</p>
+                </div>
+
+                <div className='chat-header-icons'>
+                    <BsCameraVideo className='chat-header-icon'/>
+                    <BsTelephone className='chat-header-icon'/>
+                </div>
+            </div>
         </div>
 
         <InboxFooter/>
